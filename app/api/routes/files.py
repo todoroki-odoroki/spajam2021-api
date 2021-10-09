4d847f700ed3aa3a02dacd7468cf889cfbf2632e
HideBa <baba.papa1120.ba@gmail.com>
2021-10-10 00:44:11 +0900

    feat: upload file to s3

--- app/api/routes/files.py
+++ app/api/routes/files.py
@@ -1,17 +1,37 @@
-from app.core.config import AWS_SECRET_ACCESS_KEY
+from app.core.config import AWS_BUCKET_NAME, AWS_SECRET_ACCESS_KEY
 from fastapi import APIRouter, File, UploadFile
-from app.dependencies.aws import s3
+from app.dependencies.aws import s3, bucket, s3_client
+import datetime
+import os
+from botocore.exceptions import ClientError
+
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
 
 router = APIRouter()
 
 
 @router.post("/")
 async def create_file(file: bytes = File(...)):
-    buckets = s3.list_buckets()
-    print(s3)
-    print("buckets--------", buckets)
+    print("start--------")
+    # bucket = s3.Bucket(AWS_BUCKET_NAME)
+    print("bucket ---------------")
+    print(bucket)
+    # bucket.upload_fileobj(file, "test")
     return {"file_size": len(file)}
+
 
 @router.post("/upload")
 async def create_upload_file(file: UploadFile = File(...)):
+    filename = file.filename
+    JST = datetime.timezone(datetime.timedelta(hours=+9), "JST")
+    current_time = datetime.datetime.now(JST)
+    split_file_name = os.path.splitext(filename)
+    file_name_unique = str(current_time.timestamp()).replace(".", "")
+    file_extension = split_file_name[1]
+    data = file.file._file
+    try:
+        response = s3_client.upload_fileobj(data, AWS_BUCKET_NAME, file_name_unique + split_file_name[0])
+        print("res--------", response)
+    except ClientError as e:
+        print("err-----------", e)
     return {"filename": file.filename}
--- app/dependencies/aws.py
+++ app/dependencies/aws.py
@@ -1,4 +1,6 @@
 import boto3
-from app.core.config import AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from app.core.config import AWS_BUCKET_NAME, AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
-s3 = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+s3 = boto3.resource("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+s3_client = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+bucket = s3.Bucket(AWS_BUCKET_NAME)
